f2303152925f620dfeafe46fbf7d4c47aee416fc
Ryan Liu <rliu@medrio.com>
2020-09-01 15:54:25 -0700

    remove line starts with

--- SmallTools2/Org/RyanLiu/SmallTools/BreakLinesForm.cs
+++ SmallTools2/Org/RyanLiu/SmallTools/BreakLinesForm.cs
@@ -35,9 +35,9 @@ namespace Org.RyanLiu.SmallTools
         private Label label1;
         private Button btnFormatException;
         private Button btnToOneLine;
-        private Label label2;
         private Button btnMoveResultToLeft;
-        private Label label3;
+        private Button btnRemoveLineStartsWith;
+        private Label label2;
 
         /// <summary>
         /// Required designer variable.
@@ -98,9 +98,9 @@ namespace Org.RyanLiu.SmallTools
             this.label1 = new System.Windows.Forms.Label();
             this.btnFormatException = new System.Windows.Forms.Button();
             this.btnToOneLine = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.btnMoveResultToLeft = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnRemoveLineStartsWith = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.pnlBottom.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -108,11 +108,11 @@ namespace Org.RyanLiu.SmallTools
             // 
             this.tbxSrc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.tbxSrc.Location = new System.Drawing.Point(12, 54);
+            this.tbxSrc.Location = new System.Drawing.Point(14, 62);
             this.tbxSrc.Multiline = true;
             this.tbxSrc.Name = "tbxSrc";
             this.tbxSrc.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tbxSrc.Size = new System.Drawing.Size(347, 382);
+            this.tbxSrc.Size = new System.Drawing.Size(417, 366);
             this.tbxSrc.TabIndex = 0;
             // 
             // tbxBroke
@@ -120,53 +120,53 @@ namespace Org.RyanLiu.SmallTools
             this.tbxBroke.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbxBroke.Location = new System.Drawing.Point(465, 48);
+            this.tbxBroke.Location = new System.Drawing.Point(558, 55);
             this.tbxBroke.Multiline = true;
             this.tbxBroke.Name = "tbxBroke";
             this.tbxBroke.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tbxBroke.Size = new System.Drawing.Size(571, 388);
+            this.tbxBroke.Size = new System.Drawing.Size(475, 373);
             this.tbxBroke.TabIndex = 1;
             // 
             // btnBreaak
             // 
-            this.btnBreaak.Location = new System.Drawing.Point(364, 64);
+            this.btnBreaak.Location = new System.Drawing.Point(442, 105);
             this.btnBreaak.Name = "btnBreaak";
-            this.btnBreaak.Size = new System.Drawing.Size(92, 49);
+            this.btnBreaak.Size = new System.Drawing.Size(110, 56);
             this.btnBreaak.TabIndex = 2;
-            this.btnBreaak.Text = "&Break to multiple lines by >";
+            this.btnBreaak.Text = ">&Break to multiple lines";
             this.btnBreaak.Click += new System.EventHandler(this.btnBreaak_Click);
             // 
             // tbxSplitStr
             // 
-            this.tbxSplitStr.Location = new System.Drawing.Point(391, 119);
+            this.tbxSplitStr.Location = new System.Drawing.Point(459, 22);
             this.tbxSplitStr.Name = "tbxSplitStr";
-            this.tbxSplitStr.Size = new System.Drawing.Size(54, 20);
+            this.tbxSplitStr.Size = new System.Drawing.Size(65, 22);
             this.tbxSplitStr.TabIndex = 4;
             this.tbxSplitStr.Text = "at ";
             // 
             // btnClearSrc
             // 
-            this.btnClearSrc.Location = new System.Drawing.Point(12, 14);
+            this.btnClearSrc.Location = new System.Drawing.Point(14, 16);
             this.btnClearSrc.Name = "btnClearSrc";
-            this.btnClearSrc.Size = new System.Drawing.Size(64, 24);
+            this.btnClearSrc.Size = new System.Drawing.Size(77, 28);
             this.btnClearSrc.TabIndex = 5;
             this.btnClearSrc.Text = "C&lear";
             this.btnClearSrc.Click += new System.EventHandler(this.btnClearSrc_Click);
             // 
             // btnClearResult
             // 
-            this.btnClearResult.Location = new System.Drawing.Point(541, 3);
+            this.btnClearResult.Location = new System.Drawing.Point(692, 3);
             this.btnClearResult.Name = "btnClearResult";
-            this.btnClearResult.Size = new System.Drawing.Size(40, 24);
+            this.btnClearResult.Size = new System.Drawing.Size(66, 28);
             this.btnClearResult.TabIndex = 6;
             this.btnClearResult.Text = "Clear";
             this.btnClearResult.Click += new System.EventHandler(this.btnClearResult_Click);
             // 
             // btnCopytAll
             // 
-            this.btnCopytAll.Location = new System.Drawing.Point(452, 3);
+            this.btnCopytAll.Location = new System.Drawing.Point(585, 3);
             this.btnCopytAll.Name = "btnCopytAll";
-            this.btnCopytAll.Size = new System.Drawing.Size(73, 24);
+            this.btnCopytAll.Size = new System.Drawing.Size(88, 28);
             this.btnCopytAll.TabIndex = 7;
             this.btnCopytAll.Text = "Copy All";
             this.btnCopytAll.Click += new System.EventHandler(this.btnCopyAll_Click);
@@ -174,9 +174,9 @@ namespace Org.RyanLiu.SmallTools
             // btnClose
             // 
             this.btnClose.BackColor = System.Drawing.SystemColors.Control;
-            this.btnClose.Location = new System.Drawing.Point(597, 3);
+            this.btnClose.Location = new System.Drawing.Point(790, 3);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(65, 24);
+            this.btnClose.Size = new System.Drawing.Size(78, 28);
             this.btnClose.TabIndex = 8;
             this.btnClose.Text = "&Close";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -186,27 +186,27 @@ namespace Org.RyanLiu.SmallTools
             // 
             this.tbxAddText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbxAddText.Location = new System.Drawing.Point(541, 18);
+            this.tbxAddText.Location = new System.Drawing.Point(649, 21);
             this.tbxAddText.Name = "tbxAddText";
-            this.tbxAddText.Size = new System.Drawing.Size(344, 20);
+            this.tbxAddText.Size = new System.Drawing.Size(203, 22);
             this.tbxAddText.TabIndex = 9;
             this.tbxAddText.Text = "alter table ";
             // 
             // btnAddAfter
             // 
             this.btnAddAfter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAddAfter.Location = new System.Drawing.Point(889, 16);
+            this.btnAddAfter.Location = new System.Drawing.Point(857, 18);
             this.btnAddAfter.Name = "btnAddAfter";
-            this.btnAddAfter.Size = new System.Drawing.Size(64, 24);
+            this.btnAddAfter.Size = new System.Drawing.Size(77, 28);
             this.btnAddAfter.TabIndex = 10;
             this.btnAddAfter.Text = "Add After";
             this.btnAddAfter.Click += new System.EventHandler(this.btnAddAfter_Click);
             // 
             // btnAddBefore
             // 
-            this.btnAddBefore.Location = new System.Drawing.Point(438, 18);
+            this.btnAddBefore.Location = new System.Drawing.Point(539, 17);
             this.btnAddBefore.Name = "btnAddBefore";
-            this.btnAddBefore.Size = new System.Drawing.Size(87, 24);
+            this.btnAddBefore.Size = new System.Drawing.Size(104, 27);
             this.btnAddBefore.TabIndex = 11;
             this.btnAddBefore.Text = "Add Before";
             this.btnAddBefore.Click += new System.EventHandler(this.btnAddBefore_Click);
@@ -214,9 +214,9 @@ namespace Org.RyanLiu.SmallTools
             // cbxRemoveIt
             // 
             this.cbxRemoveIt.AutoSize = true;
-            this.cbxRemoveIt.Location = new System.Drawing.Point(373, 145);
+            this.cbxRemoveIt.Location = new System.Drawing.Point(447, 165);
             this.cbxRemoveIt.Name = "cbxRemoveIt";
-            this.cbxRemoveIt.Size = new System.Drawing.Size(74, 17);
+            this.cbxRemoveIt.Size = new System.Drawing.Size(93, 21);
             this.cbxRemoveIt.TabIndex = 12;
             this.cbxRemoveIt.Text = "Remvoe it";
             this.cbxRemoveIt.UseVisualStyleBackColor = true;
@@ -227,61 +227,61 @@ namespace Org.RyanLiu.SmallTools
             this.pnlBottom.Controls.Add(this.btnCopytAll);
             this.pnlBottom.Controls.Add(this.btnClose);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 456);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 451);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(1049, 33);
+            this.pnlBottom.Size = new System.Drawing.Size(1049, 38);
             this.pnlBottom.TabIndex = 13;
             // 
             // btnTakeLeft
             // 
-            this.btnTakeLeft.Location = new System.Drawing.Point(365, 274);
+            this.btnTakeLeft.Location = new System.Drawing.Point(442, 202);
             this.btnTakeLeft.Name = "btnTakeLeft";
-            this.btnTakeLeft.Size = new System.Drawing.Size(81, 24);
+            this.btnTakeLeft.Size = new System.Drawing.Size(97, 28);
             this.btnTakeLeft.TabIndex = 14;
-            this.btnTakeLeft.Text = "Take Left >";
+            this.btnTakeLeft.Text = ">Take Left";
             this.btnTakeLeft.Click += new System.EventHandler(this.btnTakeLeft_Click);
             // 
             // btnTakeRight
             // 
-            this.btnTakeRight.Location = new System.Drawing.Point(365, 304);
+            this.btnTakeRight.Location = new System.Drawing.Point(442, 236);
             this.btnTakeRight.Name = "btnTakeRight";
-            this.btnTakeRight.Size = new System.Drawing.Size(81, 24);
+            this.btnTakeRight.Size = new System.Drawing.Size(97, 27);
             this.btnTakeRight.TabIndex = 15;
-            this.btnTakeRight.Text = "Take Right >";
+            this.btnTakeRight.Text = ">Take Right";
             this.btnTakeRight.Click += new System.EventHandler(this.btnTakeRight_Click);
             // 
             // btnLinesAddBefore
             // 
-            this.btnLinesAddBefore.Location = new System.Drawing.Point(364, 367);
+            this.btnLinesAddBefore.Location = new System.Drawing.Point(437, 330);
             this.btnLinesAddBefore.Name = "btnLinesAddBefore";
-            this.btnLinesAddBefore.Size = new System.Drawing.Size(81, 24);
+            this.btnLinesAddBefore.Size = new System.Drawing.Size(97, 28);
             this.btnLinesAddBefore.TabIndex = 16;
-            this.btnLinesAddBefore.Text = "add before >";
+            this.btnLinesAddBefore.Text = ">Add before";
             this.btnLinesAddBefore.Click += new System.EventHandler(this.btnLinesAddBefore_Click);
             // 
             // btnLinesAddAfter
             // 
-            this.btnLinesAddAfter.Location = new System.Drawing.Point(366, 397);
+            this.btnLinesAddAfter.Location = new System.Drawing.Point(439, 364);
             this.btnLinesAddAfter.Name = "btnLinesAddAfter";
-            this.btnLinesAddAfter.Size = new System.Drawing.Size(81, 24);
+            this.btnLinesAddAfter.Size = new System.Drawing.Size(97, 28);
             this.btnLinesAddAfter.TabIndex = 17;
-            this.btnLinesAddAfter.Text = "add after >";
+            this.btnLinesAddAfter.Text = ">Add after";
             this.btnLinesAddAfter.Click += new System.EventHandler(this.btnLinesAddAfter_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 351);
+            this.label1.Location = new System.Drawing.Point(436, 310);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(118, 17);
             this.label1.TabIndex = 18;
-            this.label1.Text = "add Line to Line >";
+            this.label1.Text = ">add Line to Line";
             // 
             // btnFormatException
             // 
-            this.btnFormatException.Location = new System.Drawing.Point(92, 15);
+            this.btnFormatException.Location = new System.Drawing.Point(110, 17);
             this.btnFormatException.Name = "btnFormatException";
-            this.btnFormatException.Size = new System.Drawing.Size(109, 24);
+            this.btnFormatException.Size = new System.Drawing.Size(131, 28);
             this.btnFormatException.TabIndex = 19;
             this.btnFormatException.Text = "Format &Exception text";
             this.btnFormatException.Click += new System.EventHandler(this.btnFormatException_Click);
@@ -289,49 +289,50 @@ namespace Org.RyanLiu.SmallTools
             // btnToOneLine
             // 
             this.btnToOneLine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnToOneLine.Location = new System.Drawing.Point(957, 16);
+            this.btnToOneLine.Location = new System.Drawing.Point(938, 18);
             this.btnToOneLine.Name = "btnToOneLine";
-            this.btnToOneLine.Size = new System.Drawing.Size(88, 24);
+            this.btnToOneLine.Size = new System.Drawing.Size(106, 28);
             this.btnToOneLine.TabIndex = 20;
             this.btnToOneLine.Text = "To One line";
             this.btnToOneLine.Click += new System.EventHandler(this.btnToOneLine_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(390, 258);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
-            this.label2.TabIndex = 21;
-            this.label2.Text = "to";
-            // 
             // btnMoveResultToLeft
             // 
-            this.btnMoveResultToLeft.Location = new System.Drawing.Point(366, 205);
+            this.btnMoveResultToLeft.Location = new System.Drawing.Point(439, 412);
             this.btnMoveResultToLeft.Name = "btnMoveResultToLeft";
-            this.btnMoveResultToLeft.Size = new System.Drawing.Size(87, 35);
+            this.btnMoveResultToLeft.Size = new System.Drawing.Size(105, 26);
             this.btnMoveResultToLeft.TabIndex = 22;
             this.btnMoveResultToLeft.Text = "< Move Result to Left ";
             this.btnMoveResultToLeft.UseVisualStyleBackColor = true;
             this.btnMoveResultToLeft.Click += new System.EventHandler(this.btnMoveResultToLeft_Click);
             // 
-            // label3
+            // btnRemoveLineStartsWith
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(392, 189);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 13);
-            this.label3.TabIndex = 23;
-            this.label3.Text = "then";
+            this.btnRemoveLineStartsWith.Location = new System.Drawing.Point(442, 47);
+            this.btnRemoveLineStartsWith.Name = "btnRemoveLineStartsWith";
+            this.btnRemoveLineStartsWith.Size = new System.Drawing.Size(115, 52);
+            this.btnRemoveLineStartsWith.TabIndex = 24;
+            this.btnRemoveLineStartsWith.Text = ">Remove Lines start with";
+            this.btnRemoveLineStartsWith.UseVisualStyleBackColor = true;
+            this.btnRemoveLineStartsWith.Click += new System.EventHandler(this.btnRemoveLineStartsWith_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Keyword";
             // 
             // BreakLinesForm
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
             this.ClientSize = new System.Drawing.Size(1049, 489);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRemoveLineStartsWith);
             this.Controls.Add(this.cbxRemoveIt);
             this.Controls.Add(this.btnMoveResultToLeft);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnToOneLine);
             this.Controls.Add(this.tbxSplitStr);
             this.Controls.Add(this.btnFormatException);
@@ -550,6 +551,30 @@ namespace Org.RyanLiu.SmallTools
         {
             this.tbxSrc.Text = this.tbxBroke.Text;
             this.tbxBroke.Clear();
+        }
+
+        private void btnRemoveLineStartsWith_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string splitStr = this.tbxSplitStr.Text;
+                if (splitStr == null || splitStr == "")
+                {
+                    MessageBox.Show("Give start with string!");
+                    return;
+                }
+
+                string[] lines = this.tbxSrc.Lines;
+                var q = from line in lines where !line.StartsWith(splitStr) 
+                        && !string.IsNullOrWhiteSpace(line)
+                        select line;
+
+                this.tbxBroke.Text = string.Join("\r\n", q);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
